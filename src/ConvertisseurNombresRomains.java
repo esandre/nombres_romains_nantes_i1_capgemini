3f6b66957972a130b1f191a63e54ed68ef78bb89
Enzo Sandré <atelier@enzosandre.fr>
2022-10-25 11:19:06 +0200

    Itération jusqu'à 5 incomplet

--- src/ConvertisseurNombresRomains.java
+++ src/ConvertisseurNombresRomains.java
@@ -1,2 +1,12 @@
-package PACKAGE_NAME;public class ConvertisseurNombresRomains {
+public abstract class ConvertisseurNombresRomains {
+    public static String
+    Convertir(int nombreArabe)
+            throws Exception {
+        if(nombreArabe <= 3)
+            return "I".repeat(nombreArabe);
+
+        if(nombreArabe == 4) return "IV";
+
+        throw new Exception();
+    }
 }
--- test/NombresRomainsTest.java
+++ test/NombresRomainsTest.java
@@ -1,2 +1,58 @@
-package PACKAGE_NAME;public class NombresRomainsTest {
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junitpioneer.jupiter.params.ShortRangeSource;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class NombresRomainsTest {
+    @DisplayName(
+            "ETANT DONNE un convertisseur de nombres romains " +
+                    "QUAND on lui donne le nombre <nombreArabe> " +
+                    "ALORS on obtient <nombreArabe> fois 'I'"
+    )
+    @ParameterizedTest
+    @ShortRangeSource(from = 1, to = 3, closed = true)
+    public void TestUnités(short nombreArabe) throws Exception {
+        // ETANT DONNE un convertisseur de nombres romains
+        // QUAND on lui donne le nombre <nombreArabe>
+        var resultat =
+                ConvertisseurNombresRomains
+                        .Convertir(nombreArabe);
+
+        // ALORS on obtient <nombreArabe> fois 'I'
+        assertEquals("I".repeat(nombreArabe), resultat);
+    }
+
+    @Test
+    @DisplayName(
+            "ETANT DONNE un convertisseur de nombres romains " +
+                    "QUAND on lui donne le nombre 4 " +
+                    "ALORS on obtient 'IV'")
+    public void Test4() throws Exception {
+        // ETANT DONNE un convertisseur de nombres romains
+        // QUAND on lui donne le nombre 4
+        var resultat =
+                ConvertisseurNombresRomains
+                        .Convertir(4);
+
+        // ALORS on obtient 'IV'
+        assertEquals("IV", resultat);
+    }
+
+    @Test
+    @DisplayName(
+            "ETANT DONNE un convertisseur de nombres romains " +
+            "QUAND on lui donne le nombre 5 " +
+            "ALORS on obtient 'V'")
+    public void Test5() throws Exception {
+        // ETANT DONNE un convertisseur de nombres romains
+        // QUAND on lui donne le nombre 5
+        var resultat =
+                ConvertisseurNombresRomains
+                        .Convertir(4);
+
+        // ALORS on obtient 'V'
+        assertEquals("V", resultat);
+    }
 }
